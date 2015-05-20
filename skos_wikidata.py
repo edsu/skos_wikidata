503af7d81b77a96e63b1670f63c1d3769c1e86b2
Ed Summers <ehs@pobox.com>
2015-05-20 16:25:50 -0400

    skosxl support

--- skos_wikidata.py
+++ skos_wikidata.py
@@ -7,27 +7,41 @@ from rdflib import Graph, Namespace
 from wikidata_suggest import suggest, Quit
 
 SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
+SKOSXL = Namespace("http://www.w3.org/2008/05/skos-xl#")
 WIKIDATA = Namespace("http://www.wikidata.org/entity/")
+NS = {"skos": SKOS, "skosxl": SKOSXL} 
 
-def match(rdf_filename, relations_prompt=False):
+def match(rdf_filename, relations_prompt=False, skosxl=False):
     G = Graph()
     G.parse(rdf_filename)
+
+    # if skosxl is being used we need to look up the labels
+    # slightly differently than in vanilla skos
+
+    if skosxl:
+        label_query = \
+            """
+            ?concept skosxl:prefLabel ?labelResource .
+            ?labelResource skosxl:literalForm ?label .
+            """
+    else:
+        label_query = """?concept skos:prefLabel ?label ."""
 
     q = """
         SELECT ?concept ?label ?close ?exact ?broad ?narrow ?related
         WHERE {
             ?concept a skos:Concept .
-            ?concept skos:prefLabel ?label .
-            OPTIONAL {?concept skos:closeMatch ?close . }
-            OPTIONAL {?concept skos:exactMatch ?exact . }
-            OPTIONAL {?concept skos:broadMatch ?broad . }
-            OPTIONAL {?concept skos:narrowMatch ?narrow. }
-            OPTIONAL {?concept skos:relatedMatch ?related . }
+            %s
+            OPTIONAL { ?concept skos:closeMatch ?close . }
+            OPTIONAL { ?concept skos:exactMatch ?exact . }
+            OPTIONAL { ?concept skos:broadMatch ?broad . }
+            OPTIONAL { ?concept skos:narrowMatch ?narrow. }
+            OPTIONAL { ?concept skos:relatedMatch ?related . }
         }
-        """
+        """ % label_query
 
     count = 0
-    for concept_uri, label, c, e, b, n, r in G.query(q, initNs={"skos": SKOS}): 
+    for concept_uri, label, c, e, b, n, r in G.query(q, initNs=NS):
         
         # no need to map again
         if c or e or b or n or r:
@@ -73,10 +87,9 @@ def pick_rel(l1, l2):
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="add Wikidata links to your SKOS")
     parser.add_argument("filename", type=str, help="path to your SKOS RDF")
     parser.add_argument("--relations-prompt", dest="relations_prompt", action="store_true", default=False, help="prompt for the type of SKOS mapping relation to use, otherwise skos:exactMatch will be used")
+    parser.add_argument("--skosxl", dest="skosxl", action="store_true", default=False, help="use SKOSXL namespace for labels instead of SKOS")
     args = parser.parse_args()
-
-    match(args.filename, args.relations_prompt)
+    match(args.filename, args.relations_prompt, args.skosxl)
